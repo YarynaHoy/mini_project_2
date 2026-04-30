e8e33996a00413132dbd1d99ac0e88601baf128d
Yaryna <yarynka.hoy@gmail.com>
2026-04-30 20:14:03 +0300

    add first task module2

---
+++ module2.py
@@ -0,0 +1,11 @@
+# module 2 assientment 1
+
+# 1
+class Bike:
+    def __init__(self, s, f):
+        self.color = s
+        self.price = f
+
+testOne = Bike('blue', 89.99)
+testTwo = Bike('purple', 25.0)
+
